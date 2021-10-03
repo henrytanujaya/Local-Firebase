8af46dfdde993e87d6a3e02964d8580c19a6c520
henrytanujaya <70674841+henrytanujaya@users.noreply.github.com>
2021-10-03 18:59:39 +0700

    Firebase

---
+++ IncrementalGame - Firebase/Assets/Scripts/LoadingController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingController : MonoBehaviour
+{
+    [SerializeField] private Button localButton, cloudButton;
+    
+    private void Start()
+    {
+        //aksi yang dilakukan ketika localbutton di pencet
+        localButton.onClick.AddListener(() =>
+        {
+            SetButtonInteractable(false); //disable button agar tidak spam
+            UserDataManager.LoadFromLocal();
+            SceneManager.LoadScene(1);
+        });
+
+        //aksi yang dilakukan ketika cloudbutton di pencet
+        cloudButton.onClick.AddListener(() =>
+        {
+            SetButtonInteractable(false); //disable button agar tidak spam
+            StartCoroutine(UserDataManager.LoadFromCloud(() => SceneManager.LoadScene(1)));
+        });
+    }
+
+    private void SetButtonInteractable(bool interactable)
+    {
+        localButton.interactable = interactable;
+        cloudButton.interactable = interactable;
+    }
+}
---
+++ IncrementalGame - Firebase/Assets/Scripts/UserDataManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase.Storage;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class UserDataManager
+{
+    private const string PROGRESS_KEY = "Progress";
+    public static UserProgressData Progress;
+
+    public static void LoadFromLocal ()
+    {
+        if (!PlayerPrefs.HasKey (PROGRESS_KEY))
+        {
+            Progress = new UserProgressData ();
+            Save (true);
+        }
+
+        else
+        {
+            string json = PlayerPrefs.GetString (PROGRESS_KEY);
+            Progress = JsonUtility.FromJson<UserProgressData> (json);
+        }
+    }
+
+    public static IEnumerator LoadFromCloud(System.Action OnComplete)
+    {
+        StorageReference targetStorage = GetTargetCloudStorage();
+
+        bool isCompleted = false, isSuccessful = false;
+        const long maxAllowedSize = 1024 * 1024;
+        targetStorage.GetBytesAsync(maxAllowedSize).ContinueWith(
+                (Task<byte[]> task) =>
+                {
+                    if (!task.IsFaulted)
+                    {
+                        string json = Encoding.Default.GetString(task.Result);
+                        Progress = JsonUtility.FromJson<UserProgressData>(json);
+                        isSuccessful = true;
+                    }
+
+                    isCompleted = true;
+                }
+            );
+
+        while (!isCompleted)
+        {
+            yield return null;
+        }
+
+        if (isSuccessful)
+        {
+            Save();
+        }
+        else //gagal artinya tidak ada data di cloud, jadi load dari data local
+        {
+            LoadFromLocal();
+        }
+
+        OnComplete?.Invoke();
+    }
+
+    private static StorageReference GetTargetCloudStorage()
+    {
+        //ambil device unique id sebagai nama file
+        string deviceID = SystemInfo.deviceUniqueIdentifier;
+        FirebaseStorage storage = FirebaseStorage.DefaultInstance;
+
+        return storage.GetReferenceFromUrl($"{storage.RootReference}/{deviceID}");
+    }
+
+    public static void Save (bool uploadToCloud = false)
+    {
+        string json = JsonUtility.ToJson (Progress);
+        PlayerPrefs.SetString (PROGRESS_KEY, json);
+
+        if (uploadToCloud)
+        {
+            AnalyticsManager.SetUserProperties("gold", Progress.Gold.ToString());
+
+            byte[] data = Encoding.Default.GetBytes(json);
+            StorageReference targetStorage = GetTargetCloudStorage();
+
+            targetStorage.PutBytesAsync(data);
+        }
+    }
+
+    public static bool HasResources(int _index)
+    {
+        return _index + 1 <= Progress.ResourcesLevels.Count;
+    }
+}
